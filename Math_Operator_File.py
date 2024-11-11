a845634cd3141bd064ddf652b0f631b9da8fec15
haslett fernandes <shupersap@gmail.com>
2024-11-10 21:44:34 -0500

    updated_doumentation

--- Math_Operator_File.py
+++ Math_Operator_File.py
@@ -1,5 +1,5 @@
 def Match_op_Lesson():
-    math_user_inp=input("Pick from these options: addition, subtraction, multiplication, division, modulus, exponentiation").lower()
+    math_user_inp=input("Pick from these options: addition, subtraction, multiplication, division, modulus, exponential").lower()
     if math_user_inp == "addition":
         print("Addition in python utilizes the + operator to  add")
         print("Ex:5+22=27")
@@ -27,6 +27,7 @@ def Match_op_Lesson():
             elif subtraction_equation == 12:
                 print("Well done, you have succesfully understood how the subtraction operator is used!!")
                 break
+        
     
     elif math_user_inp == "multiplication":
         print("Multiplication in python utilizes the * operator to multiply")
@@ -34,10 +35,52 @@ def Match_op_Lesson():
         print("Try it yourself")
         while True:
             print("x*7=21")
-            subtraction_input=int(input("pick the number that would multiply by 7 to be 21"))
-            subtraction_equation= subtract
-            if subtraction_equation != 12:
-                print("try again and make sure its a number that subtracts to 9")
-            elif addition_equation == 12:
-                print("Well done, you have succesfully understood how the subtraction operator is used!!")
+            multiplication_input=int(input("pick the number that would multiply by 7 to be 21"))
+            multiplication_equation= multiplication_input * 7
+            if multiplication_equation != 21:
+                print("try again and make sure x is a number that multiplies to 21")
+            elif multiplication_equation == 21:
+                print("Well done, you have succesfully understood how the multiplication operator is used!!")
                 break        
+
+    elif math_user_inp == "division":
+        print("Division in python utilizes the / operator to divide")
+        print("Ex:15/5=3")
+        print("Try it yourself")
+        while True:
+            print("27/x=9")
+            division_input=int(input("pick the number that would divide 27 by x into 9"))
+            division_equation= 27 / division_input 
+            if division_equation != 3:
+                print("try again and make sure its a number that the number multiplies to 21")
+            elif division_equation == 3:
+                print("Well done, you have succesfully understood how the division operator is used!!")
+                break     
+
+    elif math_user_inp == "modulus":
+        print("Modulus in python utilizes the %" +  "to find the remainder")
+        print("Ex:15%20=5")
+        print("Try it yourself")
+        while True:
+            print("12%" + "x=2")
+            modulus_input=int(input("pick the number that when divided by x will result in a remainder of 2"))
+            modulus_equation= 12 % modulus_input
+            if modulus_equation != 2:
+                print("try again and make sure x divides into 12 to get a remainder of 2")
+            elif modulus_equation == 2:
+                print("Well done, you have succesfully understood how the modulus operator is used!!")
+                break                    
+
+    elif math_user_inp == "exponential":
+        print("Exponential in python utilizes the ** operator to raise the power")
+        print("Ex:4**3=64")
+        print("Try it yourself")
+        while True:
+            print("3**x=27")
+            modulus_input=int(input("pick the number that when raised to the power of x will result in 27"))
+            modulus_equation= 12 % modulus_input
+            if modulus_equation != 3:
+                print("try again and make sure x can be raised to the power that will yield 27")
+            elif modulus_equation == 3:
+                print("Well done, you have succesfully understood how the exponential operator is used!!")
+                break      
--- group_project.py
+++ group_project.py
@@ -1,92 +1,112 @@
+#initialize start page
 def start(option=None):
     print("Welcome to our app")
-    option = input("Signup or Signin: ").lower()
+    option = input("Signup or Signin: ").lower() #assess whether user wants to sign up or signin
+    #allow activation of sign in and sign up function
     if option == "signin":
         signin_page()
     elif option == "signup":
         signup_page()
+    #if either option is not selected, restart the start selection
     else:
         print("Make sure you type either 'Signin' or 'Signup'")
         start(option=None)  
 
-
+#signup page function initialized
 def signup_page(Username=None, Password=None):
     while True:
+        #Store user's username and password
         Username = input("Enter a username: ")
         Password = input("Create a password: ")
-
+        
+        #if field is left blank restart the process
         if not Username or not Password:
             print("Both username and password are required. Please try again.")
-            continue
+            signup_page
 
         try:
+            #open up database text file
             with open("database.txt", "r") as db:
                 stored_users = []
+                #search through each user's details  
                 for user_info in db:
                     if user_info.strip():  
+                        #split the stored data for each user to obtain the username
                         username_stored, password_stored = user_info.split(",")
                         password_stored = password_stored.strip() 
                         stored_users.append(username_stored)
 
         except FileNotFoundError:
             stored_users = []
-
+        #make sure the user doesn't use a username that already exists 
         if Username in stored_users:
             print("Username already exists.")
+
+        #if username not found, submit user and password into database
         else:
             with open("database.txt", "a") as db:
                 db.write(f"{Username}, {Password}\n")  
             print("You have successfully created your account.")
             break 
 
+#signin page function initialized
 def signin_page(Username=None, Password=None):
     while True:
+        #have users enter their credentials
         Username = input("Enter your username: ")
         Password = input("Enter your Password: ")
-
+        
+        #if the fields are left blank, they must redo the signin process
         if not Username or not Password:
             print("Both username and password are required. Please try again.")
-            continue
-
+            signin_page()
 
         try:
+            #open database file
             with open("database.txt", "r") as db:
                 stored_user = []
                 stored_pw = []
                 for user_info in db:
+                    #find the seperated password and user id within the database
                     if user_info.strip():  
                         username_stored, password_stored = user_info.split(",")
                         password_stored = password_stored.strip() 
                         stored_user.append(username_stored)
                         stored_pw.append(password_stored)
 
+            #initialize dictionary to hold username as key and password as value
             user_dict = dict(zip(stored_user, stored_pw))
 
-
+            #check to see if the value in the dictionary matches the entered data
             if Username in user_dict:
                 stored_password = user_dict[Username]
                 if Password == stored_password:
                     print("Login success!")
                     print(f"Hi, {Username}")
-                    break  
+                    break
                 else:
                     print("Wrong password.")
             else:
                 print("Username doesn't exist.")
 
+        #If there is no database information, a sign up must be made first
         except FileNotFoundError:
             print("Database file not found. Please sign up first.")
             break  
-start()
+
 
 def home_page(Username,Password):
+    #welcome users
     print("Welcome to the tuition center \n Here to help you advance in your programming journey")
+    #utilize options function
     option_display()
     while True:
         try:
+            #reminder user to pick one of the numerical options
             if menu_selection not in [1,2,3,4]: 
                 print("please choose a number between 1 and 4")
                 option_display()
+            #direct each option to an imported function from outside files
             elif menu_selection == 1:
                 Loops_Lesson()
             elif menu_selection == 2:
@@ -98,12 +118,13 @@ def home_page(Username,Password):
             elif menu_selection == 4:
                 print("After a while crocodile \n Hope to see you again")
                 break
-        
+
+        #extra layer of user error prevention
         except ValueError:
             print("Please choose a number between 1 and 4")
             option_display()
 
-   
+#initialized function to display menu selections
 def option_display():
     print("Pick a number to learn more about that topic")
     print("1:Loops \n 2:Lists \n 3:Math operators \n 4:Exit application")
@@ -113,3 +134,5 @@ def option_display():
 
 def logging():
     return
+
+start()
