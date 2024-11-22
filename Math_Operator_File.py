4504539165c2bc123a427c34a672b55c89ce272d
haslett fernandes <shupersap@gmail.com>
2024-11-22 13:06:59 -0500

    logging_created

--- Math_Operator_File.py
+++ Math_Operator_File.py
@@ -1,86 +1,174 @@
-def Match_op_Lesson():
-    math_user_inp=input("Pick from these options: addition, subtraction, multiplication, division, modulus, exponential").lower()
-    if math_user_inp == "addition":
-        print("Addition in python utilizes the + operator to  add")
+def Math_op_Lesson():
+    math_user_inp=int(input("Pick from these options:\n 1:addition \n 2:subtraction \n 3:multiplication \n 4:division \n 5:modulus\n 6:exponential\n option: "))
+    if math_user_inp == 1:
+        print("\nAddition in python utilizes the + operator to  add")
         print("Ex:5+22=27")
-        print("Try it yourself")
+        print("\nTry it yourself")
         while True:
             print("x+8=15")
-            addition_input=int(input("pick the number that would add to 15 to see if it works"))
+            addition_input=int(input("pick the number that would add to 15 to see if it works: "))
             addition_equation= addition_input+8
             if addition_equation != 15:
                 print("try again and make sure its a number that adds to 15")
             elif addition_equation == 15:
-                print("Well done, you have succesfully understood how the addition operator is used!!")
-                break
+                print("\nWell done, you have succesfully understood how the addition operator is used!!")
+                lesson_learned_option=int(input("\nPick from these options:\n 1:Learn more about the math operators,\n 2:go to the home page,\n 3:Exit the application"))
+                if lesson_learned_option == 1:
+                    Math_op_Lesson()
+                elif lesson_learned_option == 2:
+                    home_page()
+                elif lesson_learned_option == 3:
+                    sys.exit()
     
-    elif math_user_inp == "subtraction":
-        print("Subtraction in python utilizes the - operator to subtract")
+    elif math_user_inp == 2:
+        print("\nSubtraction in python utilizes the - operator to subtract")
         print("Ex:13-5=8")
-        print("Try it yourself")
+        print("\nTry it yourself")
         while True:
             print("21-x=9")
-            subtraction_input=int(input("pick the number that would subtract from 21 to be 9 to see if it works"))
+            subtraction_input=int(input("pick the number that would subtract from 21 to be 9 to see if it works: "))
             subtraction_equation= 21-subtraction_input
-            if subtraction_equation != 12:
+            if subtraction_equation != 9:
                 print("try again and make sure its a number that subtracts to 9")
-            elif subtraction_equation == 12:
-                print("Well done, you have succesfully understood how the subtraction operator is used!!")
-                break
+            elif subtraction_equation == 9:
+                print("\nWell done, you have succesfully understood how the subtraction operator is used!!")
+                lesson_learned_option=int(input("\nPick from these options:\n 1:Learn more about the math operators,\n 2:go to the home page,\n 3:Exit the application"))
+                if lesson_learned_option == 1:
+                    Math_op_Lesson()
+                elif lesson_learned_option == 2:
+                    home_page()
+                elif lesson_learned_option == 3:
+                    sys.exit()
         
     
-    elif math_user_inp == "multiplication":
-        print("Multiplication in python utilizes the * operator to multiply")
+    elif math_user_inp == 3:
+        print("\nMultiplication in python utilizes the * operator to multiply")
         print("Ex:6*4=24")
-        print("Try it yourself")
+        print("\nTry it yourself")
         while True:
             print("x*7=21")
-            multiplication_input=int(input("pick the number that would multiply by 7 to be 21"))
+            multiplication_input=int(input("pick the number that would multiply by 7 to be 21: "))
             multiplication_equation= multiplication_input * 7
             if multiplication_equation != 21:
                 print("try again and make sure x is a number that multiplies to 21")
             elif multiplication_equation == 21:
-                print("Well done, you have succesfully understood how the multiplication operator is used!!")
-                break        
+                print("\nWell done, you have succesfully understood how the multiplication operator is used!!")
+                lesson_learned_option=int(input("\nPick from these options:\n 1:Learn more about the math operators,\n 2:go to the home page,\n 3:Exit the application"))
+                if lesson_learned_option == 1:
+                    Math_op_Lesson()
+                elif lesson_learned_option == 2:
+                    home_page()
+                elif lesson_learned_option == 3:
+                    sys.exit()      
 
-    elif math_user_inp == "division":
-        print("Division in python utilizes the / operator to divide")
+    elif math_user_inp == 4:
+        print("\nDivision in python utilizes the / operator to divide")
         print("Ex:15/5=3")
-        print("Try it yourself")
+        print("\nTry it yourself")
         while True:
             print("27/x=9")
-            division_input=int(input("pick the number that would divide 27 by x into 9"))
+            division_input=int(input("pick the number that would divide 27 by x into 9: "))
             division_equation= 27 / division_input 
             if division_equation != 3:
                 print("try again and make sure its a number that the number multiplies to 21")
             elif division_equation == 3:
-                print("Well done, you have succesfully understood how the division operator is used!!")
-                break     
+                print("\nWell done, you have succesfully understood how the division operator is used!!")
+                lesson_learned_option=int(input("\nPick from these options:\n 1:Learn more about the math operators,\n 2:go to the home page,\n 3:Exit the application"))
+                if lesson_learned_option == 1:
+                    Math_op_Lesson()
+                elif lesson_learned_option == 2:
+                    home_page()
+                elif lesson_learned_option == 3:
+                    sys.exit()
 
-    elif math_user_inp == "modulus":
-        print("Modulus in python utilizes the %" +  "to find the remainder")
+    elif math_user_inp == 5:
+        print("\nModulus in python utilizes the %" +  "to find the remainder")
         print("Ex:15%20=5")
-        print("Try it yourself")
+        print("\nTry it yourself")
         while True:
             print("12%" + "x=2")
-            modulus_input=int(input("pick the number that when divided by x will result in a remainder of 2"))
+            modulus_input=int(input("pick the number that when divided by x will result in a remainder of 2: "))
             modulus_equation= 12 % modulus_input
             if modulus_equation != 2:
                 print("try again and make sure x divides into 12 to get a remainder of 2")
             elif modulus_equation == 2:
-                print("Well done, you have succesfully understood how the modulus operator is used!!")
-                break                    
+                print("\nWell done, you have succesfully understood how the modulus operator is used!!")
+                lesson_learned_option=int(input("\nPick from these options:\n 1:Learn more about the math operators,\n 2:go to the home page,\n 3:Exit the application"))
+                if lesson_learned_option == 1:
+                    Math_op_Lesson()
+                elif lesson_learned_option == 2:
+                    home_page()
+                elif lesson_learned_option == 3:
+                    sys.exit()          
 
-    elif math_user_inp == "exponential":
-        print("Exponential in python utilizes the ** operator to raise the power")
+    elif math_user_inp == 6:
+        print("\nExponential in python utilizes the ** operator to raise the power")
         print("Ex:4**3=64")
-        print("Try it yourself")
+        print("\nTry it yourself")
         while True:
             print("3**x=27")
-            modulus_input=int(input("pick the number that when raised to the power of x will result in 27"))
-            modulus_equation= 12 % modulus_input
-            if modulus_equation != 3:
+            exponential_input=int(input("pick the number that when raised to the power of x will result in 27: "))
+            exponential_equation= 12 % exponential_input
+            if exponential_equation != 3:
                 print("try again and make sure x can be raised to the power that will yield 27")
-            elif modulus_equation == 3:
-                print("Well done, you have succesfully understood how the exponential operator is used!!")
-                break      
+            elif exponential_equation == 3:
+                print("\nWell done, you have succesfully understood how the exponential operator is used!!")
+                lesson_learned_option=int(input("\nPick from these options:\n 1:Learn more about the math operators,\n 2:go to the home page,\n 3:Exit the application"))
+                if lesson_learned_option == 1:
+                    Math_op_Lesson()
+                elif lesson_learned_option == 2:
+                    home_page()
+                elif lesson_learned_option == 3:
+                    sys.exit()
+
+
+
+
+#Chatgpt version of improved code, implement later
+"""def get_math_input(prompt, correct_answer):
+    while True:
+        try:
+            user_input = int(input(prompt))
+            if user_input == correct_answer:
+                return True
+            else:
+                print("Try again, make sure your answer is correct.")
+        except ValueError:
+            print("Please enter a valid number.")
+
+def math_operator_lesson(operator, example, prompt, correct_answer):
+    print(f"\n{operator} in Python utilizes the corresponding operator.")
+    print(f"Example: {example}")
+    print("\nTry it yourself.")
+    if get_math_input(prompt, correct_answer):
+        print(f"\nWell done, you have successfully understood how the {operator} operator is used!!")
+        lesson_learned_option = int(input("\nPick from these options:\n 1: Learn more about the math operators\n 2: Go to the home page\n 3: Exit the application\n"))
+        return lesson_learned_option
+
+def Math_op_Lesson():
+    math_user_inp = int(input("Pick from these options:\n 1: Addition \n 2: Subtraction \n 3: Multiplication \n 4: Division \n 5: Modulus\n 6: Exponential\n option: "))
+    
+    if math_user_inp == 1:
+        lesson_learned_option = math_operator_lesson("Addition", "5 + 22 = 27", "What number would add to 8 to make 15? ", 7)
+    
+    elif math_user_inp == 2:
+        lesson_learned_option = math_operator_lesson("Subtraction", "13 - 5 = 8", "What number would subtract from 21 to make 9? ", 12)
+    
+    elif math_user_inp == 3:
+        lesson_learned_option = math_operator_lesson("Multiplication", "6 * 4 = 24", "What number would multiply by 7 to be 21? ", 3)
+    
+    elif math_user_inp == 4:
+        lesson_learned_option = math_operator_lesson("Division", "15 / 5 = 3", "What number would divide 27 by to make 3? ", 9)
+    
+    elif math_user_inp == 5:
+        lesson_learned_option = math_operator_lesson("Modulus", "15 % 20 = 15", "What number would divide 12 to give a remainder of 2? ", 10)
+    
+    elif math_user_inp == 6:
+        lesson_learned_option = math_operator_lesson("Exponential", "4 ** 3 = 64", "What number raised to the power of x will result in 27? ", 3)
+
+    if lesson_learned_option == 1:
+        Math_op_Lesson()
+    elif lesson_learned_option == 2:
+        home_page()
+    elif lesson_learned_option == 3:
+        sys.exit()"""
--- group_project.py
+++ group_project.py
@@ -1,3 +1,11 @@
+#import external libraries
+import sys
+
+#call functions stored within files in the current working directory's path
+from Math_Operator_File import Math_op_Lesson
+from List_Lesson_File import list_lessons
+from Loop_Lesson_File import Loops_Lesson
+
 #initialize start page
 def start(option=None):
     print("Welcome to our app")
@@ -7,9 +15,11 @@ def start(option=None):
         signin_page()
     elif option == "signup":
         signup_page()
-    #if either option is not selected, restart the start selection
+    elif option == "quit":
+        sys.exit("Hope to see you again")
+    #if either options are not selected, restart the start selection
     else:
-        print("Make sure you type either 'Signin' or 'Signup'")
+        print("Make sure you type either 'Signin' or 'Signup', or 'quit' if you want to exit the application")
         start(option=None)  
 
 #signup page function initialized
@@ -22,7 +32,7 @@ def signup_page(Username=None, Password=None):
         #if field is left blank restart the process
         if not Username or not Password:
             print("Both username and password are required. Please try again.")
-            signup_page
+            continue
 
         try:
             #open up database text file
@@ -41,16 +51,15 @@ def signup_page(Username=None, Password=None):
         #make sure the user doesn't use a username that already exists 
         if Username in stored_users:
             print("Username already exists.")
-
         #if username not found, submit user and password into database
         else:
             with open("database.txt", "a") as db:
                 db.write(f"{Username}, {Password}\n")  
-            print("You have successfully created your account.")
-            break 
+            print(f"You have successfully created your account {Username}")
+            home_page(Username,Password)
 
 #signin page function initialized
-def signin_page(Username=None, Password=None):
+def signin_page(Username=None,Password=None):
     while True:
         #have users enter their credentials
         Username = input("Enter your username: ")
@@ -59,7 +68,7 @@ def signin_page(Username=None, Password=None):
         #if the fields are left blank, they must redo the signin process
         if not Username or not Password:
             print("Both username and password are required. Please try again.")
-            signin_page()
+            continue
 
         try:
             #open database file
@@ -83,6 +92,7 @@ def signin_page(Username=None, Password=None):
                 if Password == stored_password:
                     print("Login success!")
                     print(f"Hi, {Username}")
+                    home_page(Username,Password)
                     break
                 else:
                     print("Wrong password.")
@@ -92,44 +102,106 @@ def signin_page(Username=None, Password=None):
         #If there is no database information, a sign up must be made first
         except FileNotFoundError:
             print("Database file not found. Please sign up first.")
-            break  
+            signup_page()  
 
 
-def home_page(Username,Password):
+def home_page(home_user,home_pw):
+
+    #Track user clicks on each topic through a dictionary pair
+    topic_clicks = {'Loops': 0, 'Lists': 0, 'Math operations': 0}
+    topic_clicks = load_user_data(home_user, topic_clicks) #check to see if a user has used the application before and collect that info from the database
+
     #welcome users
-    print("Welcome to the tuition center \n Here to help you advance in your programming journey")
-    #utilize options function
-    option_display()
+    print(f"Welcome to the tuition center {home_user}\nHere to help you advance in your programming journey \n")
     while True:
+        menu_selection = option_display()  # Store the result of option_display() here
         try:
             #reminder user to pick one of the numerical options
             if menu_selection not in [1,2,3,4]: 
-                print("please choose a number between 1 and 4")
-                option_display()
+                print("please choose a number between 1 and 4 \n")
+                continue
             #direct each option to an imported function from outside files
             elif menu_selection == 1:
+                topic_clicks['Loops'] += 1 
                 Loops_Lesson()
             elif menu_selection == 2:
+                topic_clicks['Lists'] += 1
                 Lists_Lesson()
             elif menu_selection == 3:
+                topic_clicks["Math operations"] += 1
                 print("So you want to learn more about math operations in python, do you")
                 print("Well we got the one stop shop for you")
                 Math_op_Lesson()
             elif menu_selection == 4:
-                print("After a while crocodile \n Hope to see you again")
+                print("After a while crocodile \nHope to see you again")
+                log_user_activity(home_user, topic_clicks)
                 break
-
         #extra layer of user error prevention
         except ValueError:
             print("Please choose a number between 1 and 4")
-            option_display()
+            continue
+
+def load_user_data(home_user, topic_clicks):
+    try:
+        # Open the database file in read mode to get existing users
+        with open("database.txt", "r") as db:
+            lines = db.readlines()
+
+        # Search for the user's line in the database and load their topic click data
+        for line in lines:
+            username_stored, password_stored, *topic_data = line.strip().split(",")
+            if username_stored == home_user:
+                # Extract topic click counts and update the dictionary
+                for topic in topic_data:
+                    topic_name, count = topic.split(":")
+                    if topic_name in topic_clicks:
+                        topic_clicks[topic_name] = int(count)
+                break
+    except FileNotFoundError:
+        print("Database file not found. New user session initiated.")
+    
+    return topic_clicks
+
+# Function to log the user's activity to the database.txt file
+def log_user_activity(home_user, topic_clicks):
+    try:
+        # Open the database file in read mode to get existing users
+        with open("database.txt", "r") as db:
+            lines = db.readlines()
+
+        # Check if the user's data already exists
+        user_found = False
+        for i, line in enumerate(lines):
+            username_stored, password_stored, *topic_data = line.strip().split(",")
+            if username_stored == home_user:
+                user_found = True
+                # Update the user's topic click data in the database
+                new_data = f"{home_user},{password_stored}," + ",".join([f"{key}:{value}" for key, value in topic_clicks.items()])
+                lines[i] = new_data + "\n"
+                break
+
+        # If the user wasn't found, add a new line with their activity
+        if not user_found:
+            new_data = f"{home_user},<password_placeholder>," + ",".join([f"{key}:{value}" for key, value in topic_clicks.items()])
+            lines.append(new_data + "\n")
+
+        # Write the updated data back to the file
+        with open("database.txt", "w") as db:
+            db.writelines(lines)
+
+    except FileNotFoundError:
+        # If the file doesn't exist, create it and add the user's data
+        with open("database.txt", "w") as db:
+            new_data = f"{home_user},<password_placeholder>," + ",".join([f"{key}:{value}" for key, value in topic_clicks.items()])
+            db.write(new_data + "\n")
+
 
 #initialized function to display menu selections
 def option_display():
     print("Pick a number to learn more about that topic")
-    print("1:Loops \n 2:Lists \n 3:Math operators \n 4:Exit application")
-    menu_selection=int(input("Which option would you like to select? "))
-    return menu_selection
+    print(" 1:Loops \n 2:Lists \n 3:Math operators \n 4:Exit application")
+    pick= int(input("Which option would you like to select? "))
+    return pick
 
 
 def logging():
